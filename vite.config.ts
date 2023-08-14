31ea74bba20b7a7d828fe2c88a2bd67a51a2beb3
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-13 18:38:52 -0600

    vite for pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://stylink-ecommerce.github.io/Stylink-Ecommerce-React',
 });
